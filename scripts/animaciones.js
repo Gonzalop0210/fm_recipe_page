4971b02eba11d8ed705e0f815b317d0671c702ba
Gonzalo Palma <gonzalopalmadev0210@gmail.com>
2024-02-01 10:02:51 -0500

    proyecto terminado

--- scripts/animaciones.js
+++ scripts/animaciones.js
@@ -1,19 +1,22 @@
-window.sr = ScrollReveal();
-sr.reveal('.header', {
-    duration: 3000,
-    origin: 'left',
-    distance: '-200px',
-});
+window.addEventListener('load', () => {
+    window.sr = ScrollReveal();
+    sr.reveal('.header', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-200px',
+        reset: true,
+    });
 
-sr.reveal('.main', {
-    duration: 3000,
-    origin: 'rigth',
-    distance: '-200px',
-});
+    sr.reveal('.main', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-200px',
+    });
 
-sr.reveal('.footer', {
-    duration: 3000,
-    origin: 'left',
-    distance: '-200px',
-    reset: true,
-});
+    sr.reveal('.footer', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-200px',
+        reset: true,
+    });
+})
